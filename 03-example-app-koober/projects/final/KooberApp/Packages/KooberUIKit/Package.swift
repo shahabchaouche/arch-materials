a4b1432f2d8834ed62e148c13a745a289a8dec08
Shahab Chaouche <shahab_74@hotmail.com>
2024-07-30 12:23:45 -0400

    Updated king fisher package to 7.12.0

--- 03-example-app-koober/projects/final/KooberApp/Packages/KooberUIKit/Package.swift
+++ 03-example-app-koober/projects/final/KooberApp/Packages/KooberUIKit/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
   dependencies: [
     .package(path: "KooberKit"),
     .package(url: "https://github.com/mxcl/PromiseKit", .exact("6.16.2")),
-    .package(url: "https://github.com/onevcat/Kingfisher.git", .exact("7.1.2")),
+    .package(url: "https://github.com/onevcat/Kingfisher.git", .exact("7.12.0")),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
